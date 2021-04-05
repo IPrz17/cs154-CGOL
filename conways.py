e08059c76afdf6ecf8fb06e3001c296c1e586f28
Eric Smrkovsky <33237852+Ericsmrk@users.noreply.github.com>
2021-04-05 15:55:59 -0700

    Added user functionality

--- conways.py
+++ conways.py
@@ -17,11 +17,35 @@ class WOL ():
     def __init__ (self, x, y, startValues):        
         self.x = x 
         self.y = y
-        self.grid = np.zeros((self.x, self.y), dtype=int)         # Creates a grid that is X by Y
 
+        # Creates a grid that is X by Y
+        self.grid = np.zeros((self.x, self.y), dtype=int)
         for i in startValues:
-            self.grid[i[1], i[0]] = 1
+            self.grid[i[1], i[0]] = 1  #this causes error pic in chat
         
         print(self.grid)                                          # Print grid
 
-conway = WOL(4,4,[[1,1], [2,3]])
+class Conway ():
+    def __init__ (self):
+        x, y = Conway.getDimensions()                   #get input, ex: 5 5
+        startValues = Conway.getStartValues()           #get input, ex: 1 1    
+        #conway = WOL(x=5, y=5, startValues=[[1,1], [2,3]])
+        conway = WOL(x, y, startValues)
+
+    def getDimensions():
+        x, y = input("Enter dimensions for the world: ").split()
+        return int(x), int(y)
+
+    def getStartValues():#gets living cells from user input, ex: 1 1...2 2...3 3 
+        startValues = []
+
+        numCells = int(input("Enter number living cells: "))
+        print("Enter living cell coordinates: ") 
+        for i in range(numCells):
+            x, y = input().split()
+            startValues.append([int(x),int(y)])
+
+        return startValues
+
+
+Conway()    #runs game
